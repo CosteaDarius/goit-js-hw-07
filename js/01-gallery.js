07481975cb6c35cf2db58a684a9b6b7dc375f725
CosteaDarius <dariuscostea068@gmail.com>
2024-11-24 18:16:15 +0200

    c

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join();
+
+galleryContainer.innerHTML = galleryMarkup;
+
+galleryContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  const isImage = e.target.classList.contains("gallery__image");
+  if (!isImage) return;
+  const imageUrl = e.target.dataset.source;
+  const istance = basicLightbox.create(
+    `<img src="${imageUrl}" width="800" height="600">`
+  );
+  istance.show();
+});
 
 console.log(galleryItems);
