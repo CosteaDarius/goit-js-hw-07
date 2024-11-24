4403668eb377ae1a1361297bc466210e03878eba
CosteaDarius <dariuscostea068@gmail.com>
2024-11-24 18:55:26 +0200

    c

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,34 +1,36 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryContainer = document.querySelector(".gallery");
+const galleryContainer = document.querySelector('.gallery');
 
 const galleryMarkup = galleryItems
   .map(({ preview, original, description }) => {
-    return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`;
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
   })
-  .join();
+  .join('');
 
 galleryContainer.innerHTML = galleryMarkup;
 
-galleryContainer.addEventListener("click", (e) => {
-  e.preventDefault();
-  const isImage = e.target.classList.contains("gallery__image");
+galleryContainer.addEventListener('click', event => {
+  event.preventDefault();
+  const isImage = event.target.classList.contains('gallery__image');
   if (!isImage) return;
-  const imageUrl = e.target.dataset.source;
-  const istance = basicLightbox.create(
-    `<img src="${imageUrl}" width="800" height="600">`
-  );
-  istance.show();
+  const imageUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${imageUrl}" width="800" height="600">
+  `);
+  instance.show();
 });
 
 console.log(galleryItems);
